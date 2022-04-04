2800bb71c75171ef0db1e7a03dbf8ed8917e342a
skyhaigu <skyhaigu@tencent.com>
2022-04-04 15:15:17 +0800

    fix connect error to datanode:8020

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set some sensible defaults
-export CORE_CONF_fs_defaultFS=${CORE_CONF_fs_defaultFS:-hdfs://`hostname -f`:8020}
+export CORE_CONF_fs_defaultFS=${CORE_CONF_fs_defaultFS:-hdfs://`hostname -f`:9000}
 
 function addProperty() {
   local path=$1
